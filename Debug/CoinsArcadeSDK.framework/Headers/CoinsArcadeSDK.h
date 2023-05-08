300591d3a3f920d789ec42dd476f7d2d7e4ac5c9
Taran <NA>
2023-05-08 14:36:05 +0800

    init repo

---
+++ Debug/CoinsArcadeSDK.framework/Headers/CoinsArcadeSDK.h
@@ -0,0 +1,18 @@
+//
+//  CoinsArcadeSDK.h
+//  CoinsArcadeSDK
+//
+//  Created by Taran on 2023/4/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoinsArcadeSDK.
+FOUNDATION_EXPORT double CoinsArcadeSDKVersionNumber;
+
+//! Project version string for CoinsArcadeSDK.
+FOUNDATION_EXPORT const unsigned char CoinsArcadeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinsArcadeSDK/PublicHeader.h>
+
+
